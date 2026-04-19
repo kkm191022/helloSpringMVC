093f9281a170f62095c57c8edb6314eb79b1e14d
sun02020534 <130562968+sun02020534@users.noreply.github.com>
2026-04-19 15:17:25 +0900

    DTO class 수정

--- src/main/java/kr/ac/hansung/cse/model/CategoryForm.java
+++ src/main/java/kr/ac/hansung/cse/model/CategoryForm.java
@@ -5,21 +5,9 @@ import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
-/**
- * 카테고리 등록 폼 데이터를 바인딩하기 위한 DTO
- */
-@Getter
-@Setter
+@Getter @Setter
 public class CategoryForm {
-
     @NotBlank(message = "카테고리 이름을 입력하세요")
-    @Size(max = 50, message = "카테고리 이름은 50자 이내여야 합니다")
+    @Size(max = 50, message = "50자 이내로 입력하세요")
     private String name;
-
-
-    public CategoryForm() {}
-
-    public CategoryForm(String name) {
-        this.name = name;
-    }
 }
